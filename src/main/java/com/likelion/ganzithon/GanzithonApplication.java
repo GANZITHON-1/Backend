4ef0b59e9b0d226836966f60029ab04b72f80029
daun <wjdekdns0218@gmail.com>
2025-11-09 13:00:53 +0900

    chore: init project

---
+++ src/main/java/com/likelion/ganzithon/GanzithonApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.ganzithon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GanzithonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GanzithonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelion/ganzithon/GanzithonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.ganzithon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GanzithonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
