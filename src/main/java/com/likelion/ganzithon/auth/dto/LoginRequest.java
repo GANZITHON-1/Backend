b75f59b5acf705109ea6598ab3bf304637c5d823
pyw424 <parkyw0424@naver.com>
2025-11-18 17:07:53 +0900

    del(auth): setter 제거

--- src/main/java/com/likelion/ganzithon/auth/dto/LoginRequest.java
+++ src/main/java/com/likelion/ganzithon/auth/dto/LoginRequest.java
@@ -1,10 +1,8 @@
 package com.likelion.ganzithon.auth.dto;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class LoginRequest {
     private String userId;    //nickname으로 로그인
     private String password;
--- src/main/java/com/likelion/ganzithon/auth/dto/SignupRequest.java
+++ src/main/java/com/likelion/ganzithon/auth/dto/SignupRequest.java
@@ -1,10 +1,8 @@
 package com.likelion.ganzithon.auth.dto;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class SignupRequest {
     private String name;
     private String email;
