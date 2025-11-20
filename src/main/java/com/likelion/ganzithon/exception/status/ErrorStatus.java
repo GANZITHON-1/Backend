adbeeb9c00bab12bb940cf02683740dc5fcdb2be
pyw424 <parkyw0424@naver.com>
2025-11-21 08:25:16 +0900

    add(status): response status 문구 추가

--- src/main/java/com/likelion/ganzithon/exception/status/ErrorStatus.java
+++ src/main/java/com/likelion/ganzithon/exception/status/ErrorStatus.java
@@ -22,6 +22,8 @@ public enum ErrorStatus {
     SIGNUP_DUPLICATE(HttpStatus.CONFLICT, "AUTH409", "이미 존재하는 이메일 또는 닉네임입니다."),    // 이메일 및 아이디 중복 체크 실패
     //로그인
     LOGIN_INVALID_PARAMETER(HttpStatus.UNAUTHORIZED, "AUTH401", "아이디 또는 비밀번호가 일치하지 않습니다."),
+    //로그아웃
+    LOGOUT_INVALID_TOKEN(HttpStatus.BAD_REQUEST, "AUTH401", "유효하지 않은 토큰입니다."),
 
     //PROFILE 4XX
     //회원정보 수정
@@ -44,7 +46,6 @@ public enum ErrorStatus {
     // S3
     S3_FILE_NOT_FOUND(HttpStatus.BAD_REQUEST, "S3_400", "업로드할 파일이 존재하지 않습니다."),
     S3_UPLOAD_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S3_500", "이미지 업로드 중 오류가 발생했습니다.");
-
 
     private final HttpStatus status;
     private final String code;
--- src/main/java/com/likelion/ganzithon/exception/status/SuccessStatus.java
+++ src/main/java/com/likelion/ganzithon/exception/status/SuccessStatus.java
@@ -15,6 +15,8 @@ public enum SuccessStatus {
     SIGNUP_SUCCESS(HttpStatus.OK, "AUTH200", "회원가입 성공"),
     //로그인
     LOGIN_SUCCESS(HttpStatus.OK, "AUTH200", "로그인 성공."),
+    //로그아웃
+    LOGOUT_SUCCESS(HttpStatus.OK, "AUTH200", "로그아웃 성공."),
 
     //POFILE
     PROFILE_UPDATE_SUCCESS(HttpStatus.OK, "PROFILE200", "성공적으로 수정되었습니다."),
