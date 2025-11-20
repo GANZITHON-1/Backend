e855da3f833998182351251d06107e7387cde528
pyw424 <parkyw0424@naver.com>
2025-11-21 08:24:00 +0900

    feat(auth): example/description 추가

--- src/main/java/com/likelion/ganzithon/auth/dto/SignupRequest.java
+++ src/main/java/com/likelion/ganzithon/auth/dto/SignupRequest.java
@@ -1,9 +1,22 @@
 package com.likelion.ganzithon.auth.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record SignupRequest(
+
+        @Schema(description = "사용자 이름", example = "홍길동")
         String name,
+
+        @Schema(description = "사용자 이메일", example = "hong@example.com")
         String email,
+
+        @Schema(description = "사용자 닉네임", example = "hong123")
         String nickname,
+
+        @Schema(description = "비밀번호", example = "test123")
         String password,
-        String passwordConfirm //비밀번호 체크
-) {}
+
+        @Schema(description = "비밀번호 확인", example = "test123")
+        String passwordConfirm
+        ) {
+}
