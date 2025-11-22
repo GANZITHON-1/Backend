594448bbf205bffcdd8b97a195d0e03fc4ec417c
pyw424 <parkyw0424@naver.com>
2025-11-22 11:18:54 +0900

    fix(map): 타임아웃 기본으로 수정

--- src/main/java/com/likelion/ganzithon/security/SecurityConfig.java
+++ src/main/java/com/likelion/ganzithon/security/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.connectTimeout(Duration.ofSeconds(1)).readTimeout(Duration.ofSeconds(1))    // 읽기 시간 초과 설정
+        return builder.connectTimeout(Duration.ofSeconds(5)).readTimeout(Duration.ofSeconds(5))    // 읽기 시간 초과 설정
                 .build();
     }
     @Bean
